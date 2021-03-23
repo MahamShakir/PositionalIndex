a5c57dd188a00764d3d106660ca51ea1a605a7fc
MahamShakir <maham.shakir6@gmail.com>
2021-03-23 23:00:59 +0500

    UI Complete

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
-import React , {useState} from 'react';
+import React , { useEffect, useState } from 'react';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
 import Grid from '@material-ui/core/Grid';
 import FindInPage from '@material-ui/icons/FindInPage';
 import Search from '@material-ui/icons/Search'
 import Typography from '@material-ui/core/Typography';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
 import 'fontsource-roboto';
+var pluralize = require('pluralize')
+
 
 function App() {
   let [input, setInput] = useState("");
-  let [result, setResult] = useState("");
-  let [pos_index, setPosIndex] = useState({})
+  let [result, setResult] = useState("Heyo");
+  let [pos_index, setPosIndex] = useState([])
 
   const getData = () => {
     fetch('data.json'
@@ -22,11 +26,9 @@ function App() {
     }
     )
       .then(function(response){
-        console.log(response)
         return response.json();
       })
       .then(function(myJson) {
-        console.log(myJson);
         setPosIndex(myJson)
       });
   }
@@ -36,43 +38,52 @@ function App() {
   }
 
   function handleClick(){
-    console.log(input);
-    getData()
+
+    console.log(pluralize.singular('horses'))
   }
 
+  useEffect(() => {
+    getData()
+  }, [])
 
   return (
-    <div className="App">
-      <div>
-        <Grid container spacing={3} alignItems="center" justify="center" style={{margin:"auto"}}>
-          <Typography variant="h3" style={{textAlign:"center" , color:"#3f51b5" , marginTop: "20%" }} >Boolean Retrieval Model</Typography>
-          <div style={{ marginTop:10 ,height:"1px" , width:"100%", backgroundColor:"gray"}}>
-          </div>
-          <Grid item xs={12} >
-          </Grid>
-          <Grid item xs={12} >
-          </Grid>
-          <Grid item  >
-            <FindInPage/>
-          </Grid>
-          <Grid item alignItems="center" justify="center" xs={8}>
-            <TextField id="search-field" label="Enter Query" fullWidth="true" onChange={handleInput} />
-          </Grid>
-          <Grid item xs={12} >
-          </Grid>
-          <Button
-          onClick={handleClick}
-          variant="contained"
-          color="primary"
-          size="medium"
-          startIcon={<Search />}>
-          Search
-          </Button>
-          <div style={{ marginTop:20 , height:"1px" , width:"100%", backgroundColor:"gray"}}>
-          </div>
+    <div>
+      <Grid container spacing={3} alignItems="center" justify="center" style={{margin:"auto"}}>
+        <Typography variant="h3" style={{textAlign:"center" , color:"#3f51b5" , marginTop: "20%" }} >Boolean Retrieval Model</Typography>
+        <div style={{ marginTop:10 ,height:"1px" , width:"100%", backgroundColor:"gray"}}>
+        </div>
+        <Grid item xs={12} >
         </Grid>
-      </div>
-      
+        <Grid item xs={12} >
+        </Grid>
+        <Grid item  >
+          <FindInPage/>
+        </Grid>
+        <Grid item alignItems="center" justify="center" xs={8}>
+          <TextField id="search-field" label="Enter Query" fullWidth="true" onChange={handleInput} />
+        </Grid>
+        <Grid item xs={12} >
+        </Grid>
+        <Button
+        onClick={handleClick}
+        variant="contained"
+        color="primary"
+        size="medium"
+        startIcon={<Search />}>
+        Search
+        </Button>
+        <div style={{ marginTop:20 , height:"1px" , width:"100%", backgroundColor:"gray"}}>
+        </div>
+
+        <Grid item xs={11} style={{marginTop:15}}>
+          <Typography variant="button" style={{color:"#3f51b5", fontWeight:"bold"}}>Result Set:</Typography>
+          <Card variant="outlined" style={{backgroundColor:"#e0e0e6"}}>
+            <CardContent>
+              <Typography variant="h5">{result}</Typography>
+            </CardContent>
+          </Card>
+        </Grid>
+      </Grid>
     </div>
   );
 }
